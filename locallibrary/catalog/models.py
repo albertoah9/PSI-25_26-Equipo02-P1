02fc49dd9ff105b9676a23e0e9b00b769ebfe225
albertoah9 <soyalbertoalonso@gmail.com>
2026-02-03 13:13:55 +0100

    Update models.py

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -1,25 +1,17 @@
 from django.db import models
 
-# Create your models here.
 class Genre(models.Model):
-    """
-    Modelo que representa un género literario (p. ej. ciencia ficción, poesía, etc.).
-    """
+    """ Modelo que representa un género literario (p. ej. ciencia ficción, poesía, etc.). """
     name = models.CharField(max_length=200, help_text="Ingrese el nombre del género (p. ej. Ciencia Ficción, Poesía Francesa etc.)")
 
     def __str__(self):
-        """
-        Cadena que representa a la instancia particular del modelo (p. ej. en el sitio de Administración)
-        """
+        """ Cadena que representa a la instancia particular del modelo (p. ej. en el sitio de Administración) """
         return self.name
-    
 
-from django.urls import reverse #Used to generate URLs by reversing the URL patterns
+from django.urls import reverse # Used to generate URLs by reversing the URL patterns
 
 class Book(models.Model):
-    """
-    Modelo que representa un libro (pero no un Ejemplar específico).
-    """
+    """ Modelo que representa un libro (pero no un Ejemplar específico). """
 
     title = models.CharField(max_length=200)
 
@@ -38,24 +30,17 @@ class Book(models.Model):
     language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
-        """
-        String que representa al objeto Book
-        """
+        """ String que representa al objeto Book """
         return self.title
 
-
     def get_absolute_url(self):
-        """
-        Devuelve el URL a una instancia particular de Book
-        """
+        """ Devuelve el URL a una instancia particular de Book """
         return reverse('book-detail', args=[str(self.id)])
     
 import uuid # Requerida para las instancias de libros únicos
 
 class BookInstance(models.Model):
-    """
-    Modelo que representa una copia específica de un libro (i.e. que puede ser prestado por la biblioteca).
-    """
+    """ Modelo que representa una copia específica de un libro (i.e. que puede ser prestado por la biblioteca). """
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text="ID único para este libro particular en toda la biblioteca")
     book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
     imprint = models.CharField(max_length=200)
@@ -73,33 +58,24 @@ class BookInstance(models.Model):
     class Meta:
         ordering = ["due_back"]
 
-
     def __str__(self):
-        """
-        String para representar el Objeto del Modelo
-        """
+        """ String para representar el Objeto del Modelo """
         return '%s (%s)' % (self.id,self.book.title)
     
 class Author(models.Model):
-    """
-    Modelo que representa un autor
-    """
+    """ Modelo que representa un autor """
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     date_of_birth = models.DateField('birth', null=True, blank=True)
     date_of_death = models.DateField('Died', null=True, blank=True)
 
     def get_absolute_url(self):
-        """
-        Retorna la url para acceder a una instancia particular de un autor.
-        """
+        """ Retorna la url para acceder a una instancia particular de un autor. """
         return reverse('author-detail', args=[str(self.id)])
 
 
     def __str__(self):
-        """
-        String para representar el Objeto Modelo
-        """
+        """ String para representar el Objeto Modelo """
         return '%s, %s' % (self.last_name, self.first_name)
     
 class Language(models.Model):
