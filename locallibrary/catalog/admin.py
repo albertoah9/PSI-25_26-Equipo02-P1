308ca3c87794206cf92302d89ba5a33e0059c336
Markillo124 <marcos.hernandezvidal@estudiante.uam.es>
2026-02-03 13:41:37 +0100

    Parte 4 tutorial

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -3,9 +3,36 @@ from django.contrib import admin
 # Register your models here.
 from .models import Author, Genre, Book, BookInstance, Language
 
-admin.site.register(Book)
-admin.site.register(Author)
+# admin.site.register(Book)
+# admin.site.register(Author)
 admin.site.register(Genre)
-admin.site.register(BookInstance)
+# admin.site.register(BookInstance)
 admin.site.register(Language)
+
+class BooksInline(admin.TabularInline):
+    model = Book
+    extra = 0
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+    extra = 0
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BooksInline]
+
+# Register the Admin classes for Book using the decorator
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status', 'due_back', 'id')
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+    inlines = [BooksInstanceInline]
+
+
 
--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -37,6 +37,14 @@ class Book(models.Model):
         """ Devuelve el URL a una instancia particular de Book """
         return reverse('book-detail', args=[str(self.id)])
     
+    def display_genre(self):
+        """
+        Creates a string for the Genre. This is required to display genre in Admin.
+        """
+        return ', '.join([ genre.name for genre in self.genre.all()[:3] ])
+    
+    display_genre.short_description = 'Genre'
+    
 import uuid # Requerida para las instancias de libros únicos
 
 class BookInstance(models.Model):
